4d5c97939cae956912e550800f01216d4a49a8d1
anushka17agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2020-08-15 22:01:35 +0530

    Designating multiple files into Methods.

--- encrypt.py
+++ encrypt.py
@@ -1,20 +1,50 @@
 from cryptography.fernet import Fernet
 import os
+
+def keygen():
+    from cryptography.fernet import Fernet
+    #generating the key and storing them in a file
+    key = Fernet.generate_key()
+    #creating the key file in which the key'll be stored
+    file = open('key.key','wb')
+    file.write(key)
+    file.close()
+
  
 
-#taking key as user input
-key1= input("enter the encyption key")
 
-with open('test_data/apikeys2.json','rb') as f:
-    data = f.read()
 
-#encrypting the data from the given file name
+def encyption():
 
-fernet = Fernet(key1)
-encrypted=fernet.encrypt(data)    
-os.remove('test_data/apikeys2.json') #deleting the file with the original api key
+    #taking key as user input
+    key1= input("enter the encyption key")
 
-with open('new.json','wb') as f:
-    f.write(encrypted)
+    with open('test_data/apikeys.json','rb') as f:
+        data = f.read()
+
+    #encrypting the data from the given file name
+
+    fernet = Fernet(key1)
+    encrypted=fernet.encrypt(data)    
+    os.remove('test_data/apikeys.json') #deleting the file with the original api key
+
+    with open('new.json','wb') as f:
+        f.write(encrypted)
+
+def decrypt():
+    key1= input("enter the encyption key")
+    with open('new.json','rb') as f:
+        data = f.read()
+    q= Fernet(key1)
+    decrypted_message = q.decrypt(data)
+
+    with open('decrypted.json','wb') as f:
+        f.write(decrypted_message)
+
+keygen()   
+encyption()
+decrypt()
+
+
 
  
--- keygen.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-import os
- #generating the key and storing them in a file
-key = Fernet.generate_key()
-#creating the key file in which the key'll be stored
-file = open('key.key','wb')
-file.write(key)
-file.close()
