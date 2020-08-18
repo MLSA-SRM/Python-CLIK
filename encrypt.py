96285074426cf03cb19c892c1711a9dce0566332
anushka17agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2020-08-18 19:17:53 +0530

    Passed the API key in a paramatrized function along with try catch

--- encrypt.py
+++ encrypt.py
@@ -2,48 +2,63 @@ from cryptography.fernet import Fernet
 import os
 
 def keygen():
-    from cryptography.fernet import Fernet
-    #generating the key and storing them in a file
-    key = Fernet.generate_key()
-    #creating the key file in which the key'll be stored
-    file = open('key.key','wb')
-    file.write(key)
-    file.close()
+    try:
 
- 
+        
+        from cryptography.fernet import Fernet
+        #generating the key and storing them in a file
+        key = Fernet.generate_key()
+        #creating the key file in which the key'll be stored
+        file = open('key.key','wb')
+        file.write(key)
+        file.close()
+        print("The key is succesfully generated.")
+    except:
+        print("The Key could not be generated!")
+
+def encyption(key1):
+
+    
+    try:
+        with open('test_data/apikeys.json','rb') as f:
+            data = f.read()
+
+        #encrypting the data from the given file name
+
+        fernet = Fernet(key1)
+        encrypted=fernet.encrypt(data)    
+        os.remove('test_data/apikeys.json') #deleting the file with the original api key
+
+        with open('new.json','wb') as f:
+            f.write(encrypted)
+    except:
+
+        print("The File could not be encrypted")        
+
+def decrypt(key1):
+    try:
+
+   
+
+            with open('new.json','rb') as f:
+                data = f.read()
+            q= Fernet(key1)
+            decrypted_message = q.decrypt(data)
+
+            with open('decrypted.json','wb') as f:
+                f.write(decrypted_message)
+    except:
+        ("This file could not be Decypted")            
+          
 
 
 
-def encyption():
 
-    #taking key as user input
-    key1= input("enter the encyption key")
-
-    with open('test_data/apikeys.json','rb') as f:
-        data = f.read()
-
-    #encrypting the data from the given file name
-
-    fernet = Fernet(key1)
-    encrypted=fernet.encrypt(data)    
-    os.remove('test_data/apikeys.json') #deleting the file with the original api key
-
-    with open('new.json','wb') as f:
-        f.write(encrypted)
-
-def decrypt():
-    key1= input("enter the encyption key")
-    with open('new.json','rb') as f:
-        data = f.read()
-    q= Fernet(key1)
-    decrypted_message = q.decrypt(data)
-
-    with open('decrypted.json','wb') as f:
-        f.write(decrypted_message)
-
-keygen()   
-encyption()
-decrypt()
+#keygen()  
+#taking key as user input 
+key1= input("enter the encyption key")
+#encyption(key1)
+decrypt(key1)
 
 
 
