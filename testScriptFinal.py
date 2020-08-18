8d81238f85443a94a15c0fd67d80a3317d4d07e9
Kushagra Gupta <guptakushagra15.10@gmail.com>
2020-08-18 15:35:43 +0530

    updated return statements

--- testScriptFinal.py
+++ testScriptFinal.py
@@ -10,12 +10,19 @@ def showRepos(username,apitoken):
         p = r.json()
         q = json.dumps(p, indent=4, sort_keys=True)
         dictVal = json.loads(q)
-        print(r)
-        print(type(dictVal))
-        print(dictVal)
+        info = str(r)
+       
+        repCode = "<Response [200]>"
+        
+        if(info==repCode):
+            return dictVal
+        else:
+            print("Sorry! Try again after passing correct credentials!")
+            return False
        
     except:
-        print("Sorry! Try again after passing correct information!")
+        print("Sorry! Try again after passing correct credentials!")
         return False
 
 showRepos("usernamehere","personalaccesstokenhere")
+
