44163b4a38bf639698663b85ba75128db9c659ae
Mihir Singh <mihirs16@gmail.com>
2020-08-19 06:14:13 +0530

    Add Utils | Update Encryption & IO
    - Added Utils for utilities
    - Updated Encryption
    - Updated IO

--- GitHubAPITest.py
+++ GitHubAPITest.py
@@ -5,21 +5,20 @@ def showRepos(username,apitoken):
     try:
         url = "https://api.github.com/users/"+username+"/repos"
         token = "Bearer "+apitoken 
-        headers = {"Authorization": token}
+        headers = {
+            "Authorization": token
+        }
         r = requests.get (url, headers=headers)
         p = r.json()
         q = json.dumps(p, indent=4, sort_keys=True)
         dictVal = json.loads(q)
         info = str(r)
-       
         repCode = "<Response [200]>"
-        
-        if(info==repCode):
+        if(info == repCode):
             return dictVal
         else:
             print("Sorry! Try again after passing correct credentials!")
             return False
-       
     except:
         print("Sorry! Try again after passing correct credentials!")
         return False
--- encrypt.py
+++ encrypt.py
@@ -1,65 +1,92 @@
+from utils import message, debug, init_default_key_storage, STORAGE_DIR
+from printy import raw_format
 from cryptography.fernet import Fernet
 import os
 
+# generating encryption key
 def keygen():
     try:
-
-        
-        from cryptography.fernet import Fernet
         #generating the key and storing them in a file
         key = Fernet.generate_key()
-        #creating the key file in which the key'll be stored
-        file = open('key.key','wb')
-        file.write(key)
-        file.close()
-        print("The key is succesfully generated.")
-    except:
-        print("The Key could not be generated!")
+        message.success("Encryption key generated successfully")
+    except Exception as e:
+        message.error('Couldn\'t generate encryption key')
+        debug.error(str(e))
+        return False
+    return key.decode('utf-8')
 
-def encyption(key1):
+# storing the generated key to default/specified filename
+def storeKey(key, filename = os.path.basename(os.getcwd())):
+    storeTo = os.path.join (STORAGE_DIR, filename) + "_keys.key"
+    if init_default_key_storage():
+        try:
+            #creating the key file in which the key'll be stored
+            file = open(storeTo,'w')
+            file.write(key)
+            file.close()
+            message.success("Key was stored to " + storeTo + " successfully")
+        except Exception as e:
+            message.error("Key couldn't be stored to " + storeTo)
+            debug.error("Key couldn't be stored to " + storeTo)
+            return False
+        return True
+    else:
+        return False
 
-    
+# retrieve key from specified filename
+def getKey(filename = os.path.basename(os.getcwd())):
+    getFrom = os.path.join (STORAGE_DIR, filename) + "_keys.key"
+    if init_default_key_storage():
+        try:
+            # retriving key file
+            file = open (getFrom, 'r')
+            key = file.read ()
+            file.close ()
+            message.success ('Loaded encryption key successfully')
+            return key
+        except:
+            message.error ('Couldn\'t load encryption key')
+            debug.error ('Couldn\'t load encryption key')
+            return False
+    else:
+        message.error ('Couldn\'t load encryption key')
+        debug.error ('Couldn\'t load encryption key')
+        return False
+
+# encrypting file using key
+def encryption(key, filename):
     try:
-        with open('test_data/apikeys.json','rb') as f:
+        with open(filename, 'rb') as f:
             data = f.read()
-
         #encrypting the data from the given file name
-
-        fernet = Fernet(key1)
+        fernet = Fernet(key)
         encrypted=fernet.encrypt(data)    
-        os.remove('test_data/apikeys.json') #deleting the file with the original api key
-
-        with open('new.json','wb') as f:
+        with open(filename,'wb') as f:
             f.write(encrypted)
-    except:
-
-        print("The File could not be encrypted")        
-
-def decrypt(key1):
+        message.success(filename + ' successfully encrypted')
+    except Exception as e:
+        message.error(filename + ' couldn\'t be encrypted')
+        debug.error(filename + ' couldn\'t be encrypted')
+        print (e)
+        return False
+    return True
+        
+def decryption(key, filename):
     try:
-
-   
-
-            with open('new.json','rb') as f:
-                data = f.read()
-            q= Fernet(key1)
-            decrypted_message = q.decrypt(data)
-
-            with open('decrypted.json','wb') as f:
-                f.write(decrypted_message)
-    except:
-        ("This file could not be Decypted")            
+        with open(filename,'rb') as f:
+            data = f.read()
+        q = Fernet(key)
+        decrypted_message = q.decrypt(data)
+        with open(filename,'wb') as f:
+            f.write(decrypted_message)
+        message.success(filename + ' successfully decrypted')
+    except Exception as e:
+        message.error(filename + ' couldn\'t be decrypted')
+        debug.error(filename + ' couldn\'t be decrypted')
+        print(e)
+        return False
+    return True
           
-
-
-
-
-#keygen()  
-#taking key as user input 
-key1= input("enter the encyption key")
-#encyption(key1)
-decrypt(key1)
-
-
-
- 
+print(storeKey(keygen()))
+encryption(getKey(), 'test_data/test_api_keys.json')
+decryption(getKey(), 'test_data/test_api_keys.json')
