7a3ccc282b684c6ce74c02165d38960a1c8cae97
anushka17agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2020-09-09 13:55:54 +0530

    update main.py
    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from encrypt import encryption, decryption
 from json_convert import  jsonToDict
 from utils import message
-
+from keys_init import get_filename_from_user, generate_encryption
 
 def main_menu():
     message.success("1)Type  ""add"" to add a key to the existing API keys. \n Type ""sub"" to Subtract a key to the existing API keys. \n Type ""mod"" to modify your Api keys \n Type ""Encrypt"" to Encrypt your Keys \n Type ""Decrypt"" to decrypt your keys")
@@ -13,9 +13,29 @@ def main_menu():
     elif (input=="mod"):
         pass
     elif (input=="Encrypt"):
-        s= jsonToDict
-        if(s == "False"):
+        filename= get_filename_from_user()
+        key= generate_encryption()
+        s= jsonToDict(filename)
+       
+        if(s == False):
+            print("The data is  encrypted")
+        else:
+            print("The data was not encrypted")
+            encryption(key, filename)
+            #print("The Json is now encrypted")    
             
     elif (input=="Decrypt"):
-        pass
+        filename= get_filename_from_user()
+        
+        s= jsonToDict(filename)
+        print(s)
+        if(s == False):
+            print("The data is  encrypted")
+            key1=message.prompt("enter the key")
+            decryption(key1, filename)
+
+        else:
+            print("The data is not encrypted")
+          
+        
 main_menu()
