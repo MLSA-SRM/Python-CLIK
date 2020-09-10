a44500d96aeae9eb5434e7dc5839084578e5f5b1
anushka17agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2020-09-10 20:19:49 +0530

    all member functions added to main.py
    all member functions added to main.py

--- main.py
+++ main.py
@@ -2,40 +2,78 @@ from encrypt import encryption, decryption
 from json_convert import  jsonToDict
 from utils import message
 from keys_init import get_filename_from_user, generate_encryption
+from menu_functions import add, subtract, modify
+from helpVersion import help, version
 
 def main_menu():
-    message.success("1)Type  ""add"" to add a key to the existing API keys. \n Type ""sub"" to Subtract a key to the existing API keys. \n Type ""mod"" to modify your Api keys \n Type ""Encrypt"" to Encrypt your Keys \n Type ""Decrypt"" to decrypt your keys")
+    message.success("1)Type  ""add"" to add a key to the existing API keys. \n Type ""sub"" to Subtract a key to the existing API keys. \n Type ""mod"" to modify your Api keys \n Type ""Encrypt"" to Encrypt your Keys \n Type ""Decrypt"" to decrypt your keys \n Type ""help""to get help\n Type ""version"" to know the current version")
     input= message.prompt("Enter your choice")
+    filename= message.prompt("Enter file name")
+    key1=message.prompt("enter the key")
     if(input=="add"):
-        pass
+        s= jsonToDict(filename)
+        
+       
+        if(s == False):
+            
+            decryption(key1, filename)
+            add(filename)
+        else:
+
+            add(filename)
+            
+        
+
     elif (input=="sub"):
-        pass
+        s= jsonToDict(filename)
+        
+       
+        if(s == False):
+            
+            decryption(key1, filename)
+        else:
+
+            subtract(filename)
+            #encryption(key1, filename)
     elif (input=="mod"):
-        pass
+        s= jsonToDict(filename)
+        
+       
+        if(s == False):
+            
+            decryption(key1, filename)
+        else:
+
+            modify(filename)
+        
     elif (input=="Encrypt"):
-        filename= get_filename_from_user()
-        key= generate_encryption()
+        
         s= jsonToDict(filename)
        
         if(s == False):
-            print("The data is  encrypted")
+            message.success("The data is  encrypted")
         else:
-            print("The data was not encrypted")
-            encryption(key, filename)
+            message.success("The data was not encrypted")
+            encryption(key1, filename)
             #print("The Json is now encrypted")    
             
     elif (input=="Decrypt"):
-        filename= get_filename_from_user()
+       
         
         s= jsonToDict(filename)
-        print(s)
+       
         if(s == False):
-            print("The data is  encrypted")
-            key1=message.prompt("enter the key")
+            #print("The data is  encrypted")
+            #key1=message.prompt("enter the key")
             decryption(key1, filename)
 
         else:
-            print("The data is not encrypted")
+            message.success("The data is not encrypted")
+    elif(input=="help"):
+        help()
+    elif(input=="version"):
+        version()      
+
           
         
 main_menu()
