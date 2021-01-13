5dddcf05143b1940446869676764303272c9699d
Ariz Siddiqui <arizsiddiqui@hotmail.com>
2021-01-13 21:01:32 +0530

    Configure for PyPI

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clik",
+    version="1.0",
+    entry_points={
+        'console_scripts':[
+            'clik=clikpython.clik:main_menu']},
+    author="MLSA SRM",
+    author_email="contact@msclubsrm.in",
+    description="A CLI tool to keep your Secret/OAuth Keys Safe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MLSA-SRM/Project-CLIK",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['printy', 'cryptography'],
+)
